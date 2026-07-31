1836b7d059d7bd9e58a24f092718701c4fdad52c
shariful-ire <shariful.ire@gmail.com>
2026-08-01 05:14:59 +0600

    install tailwind and daizy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
 
-  const [list, setList] = useState([])
-  const [item, setItem] = useState("")
-
-  const addToList = () => {
-    setList([...list, item])
-  }
+ 
 
   return (
     <div>
-      <p>{item}</p>
-
-      <input
-        type="text"
-        onChange={(e) => setItem(e.target.value)}
-        placeholder="Item"
-      />
-
-      <button onClick={addToList}>Add</button>
+          <h1 className="bg-amber-400 text-center text-5xl p-2 rounded-full">This is Header...</h1>
     </div>
   )
 }
